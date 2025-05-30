13508f46cbff8d95fee1edbeb8c61b5d2b451495
lewhfree <lewhfree>
2025-05-30 10:42:24 -0600

    moving to python instead of shell

---
+++ run.py
@@ -0,0 +1,26 @@
+from pathlib import Path
+import zipfile
+import http.server
+import socketserver
+
+zip_filename = 'zip.zip'
+
+excluded_files = {'widgetlist.xml', zip_filename, ".gitignore", ".project"}
+excluded_dirs = {'__pycache__', '.git', ".settings"}
+
+base_dir = Path('.')
+
+with zipfile.ZipFile(zip_filename, 'w', zipfile.ZIP_DEFLATED) as zipf:
+    for file_path in base_dir.rglob('*'):
+        if any(part in excluded_dirs for part in file_path.parts):
+            continue
+
+        if file_path.is_file() and file_path.name in excluded_files:
+            continue
+
+        zipf.write(file_path, file_path.relative_to(base_dir))
+
+Handler = http.server.SimpleHTTPRequestHandler
+
+with socketserver.TCPServer(("", 80), Handler) as httpd:
+    httpd.serve_forever()
