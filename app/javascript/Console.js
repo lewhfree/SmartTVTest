c6401859c9818d50c3b990955083726575ea102f
lewhfree <lewhfree>
2025-05-30 13:35:19 -0600

    Limit the number of console messages, clean up

--- app/javascript/Console.js
+++ app/javascript/Console.js
@@ -2,6 +2,19 @@ var logBox = document.createElement("div");
 logBox.style.cssText = "background:#000;color:#0f0;font:12px monospace;padding:4px;";
 document.body.appendChild(logBox);
 
+function lengthCheck(){
+	var count = 0;
+	for (var i = 0; i < logBox.childNodes.length; i++) {
+	  if (logBox.childNodes[i].nodeType === 1) { // ELEMENT_NODE
+	    count++;
+	  }
+	}
+	
+	if (count > 10){
+		logBox.removeChild(logBox.firstChild);
+	}
+}
+
 console.log = function(text){
 	var el = document.createElement("p");
 
