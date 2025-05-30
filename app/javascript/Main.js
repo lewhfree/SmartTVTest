449ff6eae3cff296663dc0defda610b105385a23
lewhfree <lewhfree>
2025-05-30 16:53:01 -0600

    better files status

--- app/javascript/Main.js
+++ app/javascript/Main.js
@@ -25,16 +25,20 @@ function getFile(file){
 	file = "file://" + file;
 	var xhr = new XMLHttpRequest();
 	xhr.open('GET', file, false);
-	try { xhr.send(); console.info(xhr.responseText); } catch(e) {console.error(e);}
+	try {
+		xhr.send();
+		console.info(xhr.status);
+		console.info(xhr.responseText); 
+	} catch(e) {
+		console.error(e);
+	}
 }
 
 function realOnload(){	
 	//var result = httpGet("http://10.0.4.6/test.txt");
 	//console.info(result.responseText);
 	//console.log(navigator.userAgent);
-	files = ['/proc/version', '/proc/cpuinfo', '/etc/issue', '/usr/bin/', '/etc/shadow', '/etc/group']; 
-	//'/etc/sudoers', '/etc/os-release', '/proc/meminfo', '/proc/uptime', '/proc/version', '/proc/loadavg'
-	// /etc/fstab, /etc/hostname, /etc/hosts, /etc/resolv.conf, /etc/profile, /etc/environment, /etc/motd, /var/log/dmesg, /var/log/messages
+	files = ['/proc/version', '/proc/cpuinfo', '/etc/issue', '/usr/bin/', '/etc/shadow', '/etc/group', '/etc/sudoers', '/etc/os-release', '/proc/meminfo', '/proc/uptime', '/proc/version', '/proc/loadavg', '/etc/fstab', '/etc/hostname', '/etc/hosts', '/etc/resolv.conf', '/etc/profile', '/etc/environment', '/etc/motd', '/var/log/dmesg', '/var/log/messages'];
 	
 	for(var i = 0; i<files.length; i++){
 		getFile(files[i]);
