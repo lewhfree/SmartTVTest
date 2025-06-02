77d8b1de5cdac826cdef56e90def12176b305762
lewhfree <lewhfree>
2025-06-02 13:55:28 -0600

    clean up css and console

--- app/javascript/osk.js
+++ app/javascript/osk.js
@@ -2,18 +2,17 @@ var shifted = True;
 
 function OSKPressed(key){
 	textBox = document.getElementById("search");
-	boxVal = textBox.value;
-	
 	if(!shifted){
 		key = key.toLowerCase();
 	}
-	boxVal += key;
+	textBox.value += key;
 }
 
 function OSKBackspace(){
 	textBox = document.getElementById("search");
 	str = textBox.value;
 	str = str.substring(0, str.length - 1);
+	textBox.value = str;
 }
 
 function OSKEnter(){
